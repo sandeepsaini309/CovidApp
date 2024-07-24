704a3a8ee712c35f15367f12a26da31d361cbfcb
Sandeep309 <sandeepsaini6237@gmail.com>
2024-07-24 16:28:11 +0530

    ADD: build and deploy cmd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  
 };
+
+// !! build cmd
+// ng build --configuration=production --base-href "https://sandeepsaini309.github.io/CovidApp/"
+
+// !! Deploy cmd
+// npx angular-cli-ghpages --dir=dist/covidApp
